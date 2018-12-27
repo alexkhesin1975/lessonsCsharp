449a17fee5ca7a748f45ba5bb275045351569a2b
AlexKhesin <alex_khesin@hotmail.com>
2018-12-27 18:19:54 +0200

    FirstLesson In C#
    FirstLesson In C#

---
+++ Lesson1/Lesson1/print to screen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hello world");
+            Console.WriteLine("that is my first self writed program");
+            Console.WriteLine("whaoo!!!");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lesson1/Lesson1/read input.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program1
+    {
+         static void Second(string[] args)
+        {
+            string input1, input2, input3;
+
+            Console.WriteLine();
+            Console.WriteLine("that is my second self writed program");
+            Console.WriteLine("whaoo!!!");
+            Console.WriteLine("please insert some text");
+            input1 = Console.ReadLine();
+            Console.WriteLine("and another one please");
+            input2 = Console.ReadLine();
+            Console.WriteLine("and last one");
+            input3 = Console.ReadLine();
+            Console.WriteLine(input1);
+            Console.WriteLine(input2);
+            Console.WriteLine(input3);
+            Console.WriteLine(input1 + input2);
+            Console.WriteLine(input2 + input3);
+            Console.WriteLine(input1 + input3);
+            Console.WriteLine(input1 + input2 + input3);
+        }
+    }
+}
