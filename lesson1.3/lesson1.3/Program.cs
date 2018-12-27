03d6c0e637a84e38e3634f9a41d929016ce54109
AlexKhesin <alex_khesin@hotmail.com>
2018-12-27 19:28:46 +0200

    some simple math count with double use
    same as 1.2 but vith double use

---
+++ lesson1.3/lesson1.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("that is my forth self writed program");
+            Console.WriteLine("Wahoo!!");
+            Console.WriteLine();
+
+            double num1, num2, num3;
+
+            Console.WriteLine("please insert 3 numbers");
+            Console.WriteLine();
+            Console.WriteLine("please insert first number");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("please insert second number");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("please insert third number");
+            num3 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("now we do sum count");
+            Console.WriteLine(num1 + num2 + num3);
+            Console.WriteLine();
+            Console.WriteLine("now we do multiplication count");
+            Console.WriteLine(num1 * num2 * num3);
+            Console.WriteLine();
+            Console.WriteLine("now we do average count");
+            Console.WriteLine((num1 + num2 + num3) / 3.0);
+            Console.WriteLine();
+            Console.WriteLine("now we do exponentiation count");
+            Console.WriteLine(num1 * num1 + "|" + num2 * num2 + "|" + num3 * num3);
+        }
+    }
+}
