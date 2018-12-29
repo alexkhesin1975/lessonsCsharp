6529e9b6c77069f320556afd51ef32d1837b3ed6
AlexKhesin <alex_khesin@hotmail.com>
2018-12-29 14:33:23 +0200

    not working
    some isunderstanding in integriters

---
+++ lesson1.11/lesson1.11/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1, input2, input3;
+
+            Console.WriteLine();
+            Console.WriteLine("that is my second self writed program");
+            Console.WriteLine("whaoo!!!");
+            Console.WriteLine("please choose color");
+            Console.WriteLine("please choose Foreground color");
+            string foregroundString = "Wite";
+            foregroundString = Console.ReadLine();
+            Type foreground = Type.GetType(foregroundString);
+            Console.WriteLine("please choose background color");
+            string backgroundString = "Black";
+            backgroundString = Console.ReadLine();
+            Type background = Type.GetType(backgroundString);
+            Console.ForegroundColor = ConsoleColor(foreground);
+            Console.BackgroundColor = ConsoleColor.background;
+
+            Console.WriteLine("please insert some text");
+            input1 = Console.ReadLine();
+            Console.WriteLine("and another one please");
+            input2 = Console.ReadLine();
+            Console.WriteLine("and last one");
+            input3 = Console.ReadLine();
+            Console.WriteLine(input1);
+            Console.WriteLine(input2);
+            Console.WriteLine(input3);
+            Console.WriteLine(input1 + "|" + input2);
+            Console.WriteLine(input2 + "|" + input3);
+            Console.WriteLine(input1 + "|" + input3);
+            Console.WriteLine(input1 + "|" + input2 + "|" + input3);
+
+            
+        }
+}
+}
