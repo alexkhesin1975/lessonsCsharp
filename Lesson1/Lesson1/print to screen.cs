50a5b85e4d3d8c6fa412309a8ec8cacede86c0b2
AlexKhesin <alex_khesin@hotmail.com>
2018-12-29 14:32:47 +0200

    ok

--- Lesson1/Lesson1/print to screen.cs
+++ Lesson1/Lesson1/print to screen.cs
@@ -10,11 +10,28 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+
             Console.WriteLine();
             Console.WriteLine("Hello world");
             Console.WriteLine("that is my first self writed program");
             Console.WriteLine("whaoo!!!");
             Console.WriteLine();
+
+            Console.ResetColor();
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+            Console.WriteLine();
+            Console.WriteLine("Hello world");
+            Console.WriteLine("that is my first self writed program");
+            Console.WriteLine("whaoo!!!");
+            Console.WriteLine();
+            Console.ResetColor();
         }
     }
 }
