e51ade049beb1faa6454bafa2ac4f5fe17bd4570
AlexKhesin <alex_khesin@hotmail.com>
2019-01-11 13:23:43 +0200

    ok

---
+++ 5.2/5.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2
+{
+    class Program
+    {
+        static void calc(char num)
+        {
+            string curLine = "16846e5casaxyunweofg86899321";
+            char x = num;
+                foreach (char num2 in curLine)
+                {
+                    if (num2 == x)
+                    {
+                        Console.ForegroundColor = System.ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = System.ConsoleColor.White;
+                    }
+                Console.Write(num2);
+            }
+            Console.WriteLine();
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            char i;
+            i = char.Parse(Console.ReadLine());
+
+            calc(i);
+        }
+    }
+}
--- Lesson5.1/Lesson5.1/Program.cs
+++ Lesson5.1/Lesson5.1/Program.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Timers;
 
 namespace Lesson5._1
 {
     class Program
     {
+        static void calc(char num)
+            {
+            string[] vs = File.ReadAllLines("D:\\Users\\sasha\\Documents\\GitHub\\home\\lessonsCsharp\\Lesson5.1\\Lesson5.1\\CalcView.txt");
+            char x = num;
+            foreach (string curLine in vs)
+            {
+                if (curLine.Contains(x))
+                {
+                    foreach (char num2 in curLine)
+                    {
+                    
+                        if (num2 == x)
+                        {
+                            Console.ForegroundColor = System.ConsoleColor.Red;
+                            Console.Write(num2);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = System.ConsoleColor.White;
+                            Console.Write(num2);
+                        }
+                        
+                    }
+                    Console.WriteLine();
+                }
+                
+                Console.WriteLine(curLine);
+            }
+            
+        }
         static void Main(string[] args)
         {
+            char i;
+            i = char.Parse(Console.ReadLine());
+            
+                calc(i);
+            
+            
+            
         }
     }
 }
